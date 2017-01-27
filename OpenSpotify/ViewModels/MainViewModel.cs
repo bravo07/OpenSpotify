79565dfb4ce408874ddd18f6fa0090108b690241
ValonK <vk@looom.at>
2017-01-27 19:35:01 +0100

    Changes

--- OpenSpotify/ViewModels/MainViewModel.cs
+++ OpenSpotify/ViewModels/MainViewModel.cs
@@ -93,6 +93,8 @@ namespace OpenSpotify.ViewModels {
                 return;
             }
 
+            ApplicationModel.DownloadCollection.Clear();
+
             var dataObject = dropInfo.Data as IDataObject;
             if (dataObject != null && dataObject.GetDataPresent(DataFormats.StringFormat, true)) {
 
