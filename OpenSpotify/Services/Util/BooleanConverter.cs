fc4e666c5e96ecbe31b8c97422c070d8cd0d56a1
ValonK <vk@looom.at>
2017-02-10 23:19:34 +0100

    Started Implementation of MusicPlayer

--- OpenSpotify/Services/Util/BooleanConverter.cs
+++ OpenSpotify/Services/Util/BooleanConverter.cs
@@ -4,38 +4,25 @@ using System.Windows.Data;
 
 namespace OpenSpotify.Services.Util {
 
-    public sealed class BooleanConverter<T> : IValueConverter {
-
-        public T TrueValue { get; set; }
+    public class BoolToValueConverter<T> : IValueConverter
+    {
         public T FalseValue { get; set; }
+        public T TrueValue { get; set; }
 
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            if (!(value is bool)) {
-                return null;
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            if (value == null) {
+                return FalseValue;
             }
             return (bool)value ? TrueValue : FalseValue;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-            if (IsEqual(value, TrueValue)) {
-                return true;
-            }
-
-            if (IsEqual(value, FalseValue)) {
-                return false;
-            }
-
-            return null;
-        }
-
-        private static bool IsEqual(object x, object y) {
-            if (Equals(x, y)) {
-                return true;
-            }
-
-            var c = x as IComparable;
-            return c?.CompareTo(y) == 0;
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value?.Equals(TrueValue) ?? false;
         }
     }
+
+    public class PlayPauseConverter : BoolToValueConverter<string> { }
+    public class SoundConverter : BoolToValueConverter<string> { }
 
 }
---
+++ OpenSpotify/ViewModels/MusicPlayerViewModel.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+using OpenSpotify.Models;
+using OpenSpotify.Services.Util;
+
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace OpenSpotify.ViewModels
+{
+    public class MusicPlayerViewModel : BaseViewModel {
+
+        public MusicPlayerViewModel(ApplicationModel applicationModel) {
+            ApplicationModel = applicationModel;
+            Initialize();
+        }
+
+        #region Fields
+
+        private ApplicationModel _applicationModel;
+        private ImageSource _soundImage;
+        private MediaElement _soundPlayerElement;
+        private double _sliderTrackValue;
+        private TimeSpan _totalTrackTime;
+        private double _sliderTrackMaximum;
+
+        #endregion
+
+        #region Properties
+
+        public ApplicationModel ApplicationModel {
+            get { return _applicationModel; }
+            set {
+                _applicationModel = value;
+                OnPropertyChanged(nameof(ApplicationModel));
+            }
+        }
+
+        public ImageSource SoundImage {
+            get { return _soundImage; }
+            set {
+                _soundImage = value;
+                OnPropertyChanged(nameof(SoundImage));
+            }
+        }
+
+        public MediaElement SoundPlayerElement {
+            get { return _soundPlayerElement; }
+            set {
+                _soundPlayerElement = value;
+                OnPropertyChanged(nameof(SoundPlayerElement));
+            }
+        }
+
+        public double SliderTrackValue {
+            get { return _sliderTrackValue; }
+            set {
+                _sliderTrackValue = value;
+                OnPropertyChanged(nameof(SliderTrackValue));
+            }
+        }
+
+        public double SliderTrackMaximum {
+            get { return _sliderTrackMaximum; }
+            set {
+                _sliderTrackMaximum = value;
+                OnPropertyChanged(nameof(SliderTrackMaximum));
+            }
+        }
+
+        public TimeSpan TotalTrackTime {
+            get { return _totalTrackTime; }
+            set {
+                _totalTrackTime = value;
+                OnPropertyChanged(nameof(TotalTrackTime));
+            }
+        }
+
+        public DispatcherTimer SoundElementTimer { get; set; }
+        #endregion
+
+        #region Commands
+
+        public CommandHandler<bool> PlayPauseCommand {
+            get {
+                return new CommandHandler<bool>(state => {
+                    if (state)
+                    {
+                        SoundPlayerElement.Pause();
+                    }
+                    else
+                    {
+                        SoundPlayerElement.Play();
+                    }
+                });
+            }
+        }
+        
+        public CommandHandler<bool> SoundCommand {
+            get {
+                return new CommandHandler<bool>(state => {
+
+                });
+            }
+        }
+
+        public CommandHandler<object> ValueChangedCommand {
+            get {
+                return new CommandHandler<object>(state => {
+
+                });
+            }
+        }
+
+        #endregion
+
+        #region Functions 
+
+        private void Initialize() {
+            SoundPlayerElement = new MediaElement();
+            SoundPlayerElement.MediaOpened += SoundPlayerElementOnMediaOpened;
+        }
+
+        private void SoundPlayerElementOnMediaOpened(object sender, RoutedEventArgs routedEventArgs) {
+            TotalTrackTime = SoundPlayerElement.NaturalDuration.TimeSpan;
+
+            SoundElementTimer = new DispatcherTimer {Interval = TimeSpan.FromSeconds(1)};
+            SoundElementTimer.Tick += SoundElementTimerTick;
+            SoundElementTimer.Start();
+        }
+
+        private void SoundElementTimerTick(object sender, EventArgs e) {
+
+            if (!(SoundPlayerElement.NaturalDuration.TimeSpan.TotalSeconds > 0)) {
+                return;
+            }
+
+            if (TotalTrackTime.TotalSeconds > 0) {
+                SliderTrackValue = SoundPlayerElement.Position.TotalSeconds / TotalTrackTime.TotalSeconds;
+            }
+        }
+
+        #endregion 
+    }
+}
